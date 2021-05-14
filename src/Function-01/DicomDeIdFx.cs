f965a8e35a9231bf63d2d9dc835cf42eefd88950
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2021-05-13 22:44:07 -0500

    Modify Azure Function to use DicomLib and call de-id function

--- src/Function-01/DicomDeIdFx.cs
+++ src/Function-01/DicomDeIdFx.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using DicomLib;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -10,14 +11,22 @@ namespace Function_01
     public static class DicomDeIdFx
     {
         [FunctionName("DicomDeIdFx")]
-        public static void Run([BlobTrigger("DicomSamplesId/{name}", Connection = "sourceBlobConnection")]Stream myBlob, string name, ILogger log)
+        public static void Run([BlobTrigger("dicom-samples-id/{name}", Connection = "sourceBlobConnection")]Stream inStream,
+            string name,
+            // Write the output to the same filename in a different container in the same storage account
+            [Blob("dicom-samples-deid/{name}", FileAccess.Write, Connection = "sourceBlobConnection")]Stream outStream,
+            ILogger log)
         {
+            log.LogInformation($"C# Blob trigger function Processed blob\n\tName:{name}\n\tSize: {inStream.Length} Bytes");
+            
             IList<string> TagKeepList = new List<string>()
             {
-                "0002,0000"
+                "0010,0020" // Patient ID
             };
 
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            IDicomLib lib = new FODicomWrapper();
+
+            lib.RemoveTags(inStream, "<replaced>", TagKeepList, new VerboseLogger(log)).CopyTo(outStream);
         }
     }
 }
---
+++ src/Function-01/VerboseLogger.cs
@@ -0,0 +1,23 @@
+﻿using DicomLib;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Function_01
+{
+	public class VerboseLogger : IVerboseWriter
+	{
+		ILogger _logger;
+
+		public VerboseLogger(ILogger logger) 
+		{
+			_logger = logger;
+		}
+
+		public void Write(string value)
+		{
+			_logger.LogDebug(value);
+		}
+	}
+}
