8e106bf0dc1a4284d85af04a14ec68302eeb6562
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2021-05-13 19:33:38 -0500

    Fix .NET Core version for DicomLib and add references. Apply code quality recommendations

---
+++ src/ConsoleTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ src/DicomLib/fodicomWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DicomLib
+{
+	public class fodicomWrapper : IDicomLib
+	{
+	}
+}
